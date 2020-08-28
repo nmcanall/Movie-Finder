a6a478144f09f5b1d19a5c94d29d4c1f67da4f9e
brian presta <brian.presta@gmail.comgit commit -m first>
2020-08-27 18:39:49 -0700

    set up model relations

--- models/index.js
+++ models/index.js
@@ -3,4 +3,14 @@ const UserData = require('./UserData')
 const Movie = require('./Movie')
 const UserRating = require('./UserRating')
 
-console.log(User,UserData,Movie,UserRating)
+User.hasOne(UserData, {foreignKey: 'user_id'})
+User.hasMany(UserRating, {foreignKey: 'user_id'})
+
+UserData.belongsTo(User, {foreignKey: 'user_id'})
+
+Movie.hasMany(UserRating, {foreignKey: 'movie_id'})
+
+UserRating.belongsTo(User, {foreignKey: 'user_id'})
+UserRating.belongsTo(Movie, {foreignKey: 'movie_id'})
+
+module.exports = {User,UserData,Movie,UserRating}
