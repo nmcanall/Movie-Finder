c38767c9c80484185345d83a155b1814f5fa8666
nmcanall <nmcanall@citadel.edu>
2020-09-01 22:23:02 -0700

    add backend session features and authorizations

--- server.js
+++ server.js
@@ -22,21 +22,21 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// // Session dependencies
-// const session = require("express-session");
-// const SequelizeStore = require("connect-session-sequelize")(session.Store);
-// const sess = {
-//     secret: "Super secret secret",
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+// Session dependencies
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sess = {
+    secret: "Super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
 
-// // Session middleware
-// app.use(session(sess));
+// Session middleware
+app.use(session(sess));
 
 // Turn on routes
 app.use(routes);
